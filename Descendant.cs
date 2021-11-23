243fdcebed54e833bd98332082bc66bf2c6408c2
Vitezslav Kanok <vitezslav.kanok@email.cz>
2021-11-23 22:03:33 +0100

    descendant - add depth and list of positions

--- Descendant.cs
+++ Descendant.cs
@@ -7,8 +7,7 @@ namespace TupleAtATime
     {
         private readonly BasicOperator _input;
         private readonly string _tagName;
-        private int _elementPosition;
-        private int _childElementPosition;
+        private List<int> _childElementPositionList;
 
         public Descendant(BasicOperator input, string tagName)
         {
@@ -27,52 +26,58 @@ namespace TupleAtATime
             {
                 // we open the input operator
                 IsOpen = true;
-                _elementPosition = 0;
-                _childElementPosition = 0;
+                _childElementPositionList = new List<int> { 0, 0, 0, 0, 0, 0 };
 
             }
 
             // for each input element
-            while (_input.MoveNext())
+            while (_input.MoveNext()) 
             {
                 XmlNode element = _input.Current;
+                int depth = 0;
+                int childElementPosition = _childElementPositionList[depth];
                 // for each child node
-                while (_childElementPosition < element.ChildNodes.Count)
+                while (childElementPosition < element.ChildNodes.Count)
                 {
-                    if (MoveNextRecurse(element.ChildNodes[_childElementPosition])) {
+                    if (MoveNextRecurse(element.ChildNodes[childElementPosition], depth))
+                    {
                         // if yes, set it as a current cursor position and return true
                         _input.Reset();
-                        Current = element.ChildNodes[_childElementPosition++];
+                        Current = element.ChildNodes[childElementPosition];
+                        _childElementPositionList[depth] += 1;
                         return true;
                     }
 
-                    _childElementPosition++;
+                    _childElementPositionList[depth] += 1;
                 }
 
-                _elementPosition++;
-                _childElementPosition = 0;
+                _childElementPositionList[depth] = 0;
             }
 
             IsOpen = false;
             return false;
         }
 
-        public bool MoveNextRecurse(XmlNode child)
+        public bool MoveNextRecurse(XmlNode child, int depth)
         {
-            int _childChildElementPosition = 0;
+            depth++;
+            int childElementPosition = _childElementPositionList[depth];
             if (child.Name == _tagName)
             {
                 // if yes, set it as a current cursor position and return true
+                _input.Reset();
                 Current = child;
+                _childElementPositionList[depth] += 1;
                 return true;
             }
-            while (_childChildElementPosition < child.ChildNodes.Count)
+            while (childElementPosition < child.ChildNodes.Count)
             {
-                if (MoveNextRecurse(child.ChildNodes[_childChildElementPosition++]))
+                if (MoveNextRecurse(child.ChildNodes[childElementPosition++], depth))
                 {
                     return true;
                 }
             }
+            _childElementPositionList[depth] = 0;
             return false;
         }
 
